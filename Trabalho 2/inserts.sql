0ccb808a01b4973510828358a9c0357c721c0c22
Catarina24 <aniratac.correia@gmail.com>
2018-05-13 16:21:30 +0100

    Aitcho que é isto

--- Trabalho 2/inserts.sql
+++ Trabalho 2/inserts.sql
@@ -1,7 +1,13 @@
-/** XDOCENTES */
-
 insert into xdocentes_tab (nr, nome, sigla, categoria, proprio, apelido, estado)
 select nr, nome, sigla, categoria, proprio, apelido, estado
 from xdocentes;
 
-/** BALIZA */
+insert into xucs_tab (codigo, designacao, sigla_uc, curso)
+select codigo, designacao, sigla_uc, curso
+from xucs;
+
+insert into xocorrencias_tab (xucs, ano_letivo, periodo, inscritos, com_frequencia, aprovados, objetivos, conteudo, departamento)
+select ref(s), ano_letivo, periodo, inscritos, com_frequencia, aprovados, objetivos, conteudo, departamento
+from xucs_tab s 
+join xocorrencias
+on s.codigo = xocorrencias.codigo;
