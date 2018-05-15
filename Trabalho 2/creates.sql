cb802033efdd07bf9a97530640cb3d6dbb0f8c80
Catarina24 <aniratac.correia@gmail.com>
2018-05-15 17:36:22 +0100

    Fixed inserts and creates

--- Trabalho 2/creates.sql
+++ Trabalho 2/creates.sql
@@ -1,133 +1,115 @@
-/* 1º */
+/** drops */
+
+drop type xdsd_t force;
+drop type xucs_t force;
+drop type xocorrencias_t force;
+drop type xocorrencias_tab_t force;
+drop type xtiposaula_t force;
+drop type xtiposaula_tab_t force;
+drop type xdocentes_t force;
+drop type xdsd_tab_t force;
+
+drop table xocorrencias_tab;
+drop table xucs_tab;
+drop table xtiposaula_tab;
+drop table xdsd_tab;
+drop table xdocentes_tab;
+
+/** types */
 
 create type xucs_t as object (
-   codigo           varchar2(9),
-   designacao       varchar2(150),
-   sigla_uc         varchar2(6),
+   codigo           varchar2(9 byte),
+   designacao       varchar2(150 byte),
+   sigla_uc         varchar2(6 byte),
    curso            number(4,0));
 /
-
-/* 2º */
-
 create type xocorrencias_t as object (
-   xucs             ref xucs_t,
-   ano_letivo       varchar2(9),
-   periodo          varchar2(2),
+   ucs              ref xucs_t,
+   codigo           varchar2(9 byte),
+   ano_letivo       varchar2(9 byte),
+   periodo          varchar2(2 byte),
    inscritos        number(38,0),
    com_frequencia   number(38,0),
    aprovados        number(38,0),
-   objetivos        varchar2(4000),
-   conteudo         varchar2(4000),
-   departamento     varchar2(6));
+   objetivos        varchar2(4000 byte),
+   conteudo         varchar2(4000 byte),
+   departamento     varchar2(6 byte));
 /
-
-/* 3º */
-
-create table xocorrencias_tab of xocorrencias_t (
-    ano_letivo not null,
-    periodo not null
-);
-/
-
-/* 4º */
-
-create type xocorrencias_tab_t as table of xocorrencias_t;
-/
-
-/*
-create type xocorrencias_tab_t as table of ref xocorrencias_t;
-/*/
-
-/* 5º */
-
-alter type xucs_t ADD ATTRIBUTE ocorrencias xocorrencias_tab_t cascade;
-/
-
-/* 6º */
-
-create table xucs_tab of xucs_t(
-    codigo primary key,
-    designacao not null
-)
-    nested table ocorrencias store as xocorrencias_nt;
-/
-
-/* 7º */
-
 create type xtiposaula_t as object (
     id          number(10,0),
-    tipo        varchar2(2),
+    tipo        varchar2(2 byte),
+    codigo      varchar2(9 byte),
+    ano_letivo  varchar2(9 byte),
+    periodo     varchar2(2 byte),
     ocorrencias ref xocorrencias_t,
     turnos      number(4,2),
     n_aulas     number,
     horas_turno number(4,2));
 /
-
-/* 8º */
-
-create table xtiposaula_tab of xtiposaula_t (
-    id primary key
-);
-/
-
-/* 9º */
-
-create type xtiposaula_tab_t as table of xtiposaula_t;
-/
-
-/*create type xtiposaula_tab_t as table of ref xtiposaula_t;
-/*/
-
-/* 10º */
-
-alter type xocorrencias_t ADD ATTRIBUTE tiposaula xtiposaula_tab_t cascade;
-/
-
-/* 11º */
-
 create type xdsd_t as object (
-   horas    number(4,2),
-   fator    number(3,2),
-   ordem    number,
-   tiposaula xtiposaula_tab_t);
+   nr           number,
+   id           number(10,0),
+   horas        number(4,2),
+   fator        number(3,2),
+   ordem        number,
+   tiposaula    ref xtiposaula_t);
 /
-
-/* 12º */
-create table xdsd_tab of xdsd_t(
-    horas not null
-)
-    nested TABLE tiposaula STORE as xtiposaula_nt;
-/
-
-/* 13º */
-
-alter type xtiposaula_t ADD ATTRIBUTE dsd ref xdsd_t cascade;
-/
-
-/* 14º */
-
 create type xdocentes_t as object(
     nr              number,
-    nome            varchar2(75),
-    sigla           varchar2(8),
+    nome            varchar2(75 byte),
+    sigla           varchar2(8 byte),
     categoria       number,
-    proprio         varchar2(25),
-    apelido         varchar2(25),
-    estado          varchar2(3),
-    dsd             ref xdsd_t);
+    proprio         varchar2(25 byte),
+    apelido         varchar2(25 byte),
+    estado          varchar2(3 byte));
 /
 
-/* 15º */
-
-create table xdocentes_tab of xdocentes_t(
-    nr primary key,
-    nome not null,
-    sigla not null,
-    estado not null
-);
+create type xocorrencias_tab_t as table of ref xocorrencias_t;
+/
+create type xtiposaula_tab_t as table of ref xtiposaula_t;
+/
+create type xdsd_tab_t as table of ref xdsd_t;
 /
 
-/* 16º */
-
+alter type xucs_t ADD ATTRIBUTE ocorrencias xocorrencias_tab_t cascade;
+/
+alter type xocorrencias_t ADD ATTRIBUTE tiposaula xtiposaula_tab_t cascade;
+/
+alter type xtiposaula_t ADD ATTRIBUTE dsd xdsd_tab_t cascade;
+/
+alter type xdocentes_t ADD ATTRIBUTE dsd xdsd_tab_t cascade;
+/
 alter type xdsd_t ADD ATTRIBUTE docentes ref xdocentes_t cascade;
+/
+
+
+/** tables */
+
+create table xdsd_tab of xdsd_t;
+/
+
+create table xucs_tab of xucs_t
+(
+    codigo primary key,
+    designacao not null
+)
+    nested table ocorrencias store as xocorrencias_nt;
+/
+create table xocorrencias_tab of xocorrencias_t
+(
+    primary key (codigo, ano_letivo, periodo)
+)
+    nested table tiposaula store as xtiposaula_nt;
+/
+create table xtiposaula_tab of xtiposaula_t
+(
+    id primary key
+)
+    nested table dsd store as xdsd_nt;
+/
+create table xdocentes_tab of xdocentes_t
+(
+    nr primary key
+)
+    nested table dsd store as xdsd_nt_2;
 /
--- Trabalho 2/inserts.sql
+++ Trabalho 2/inserts.sql
@@ -1,34 +1,16 @@
-insert into xdocentes_tab (nr, nome, sigla, categoria, proprio, apelido, estado)
-select nr, nome, sigla, categoria, proprio, apelido, estado
-from GTD10.xdocentes;
-
+-- inserts
 insert into xucs_tab (codigo, designacao, sigla_uc, curso)
 select codigo, designacao, sigla_uc, curso
 from GTD10.xucs;
 
-insert into xocorrencias_tab (xucs, ano_letivo, periodo, inscritos, com_frequencia, aprovados, objetivos, conteudo, departamento)
-select ref(s), ano_letivo, periodo, inscritos, com_frequencia, aprovados, objetivos, conteudo, departamento
+insert into xocorrencias_tab (ucs, codigo, ano_letivo, periodo, inscritos, com_frequencia, aprovados, objetivos, conteudo, departamento)
+select ref(s), s.codigo, ano_letivo, periodo, inscritos, com_frequencia, aprovados, objetivos, conteudo, departamento
 from xucs_tab s 
 join GTD10.XOCORRENCIAS
 on s.codigo = GTD10.xocorrencias.codigo;
 
-
--- Para testar (Depende dos create)
-
-update xucs_tab u
-set u.ocorrencias = cast(multiset(
-    select oco.*
-    from xocorrencias_tab oco
-    where oco.xucs.codigo = u.codigo) as xocorrencias_tab_t);
-/*
-update xucs_tab u
-set u.ocorrencias = cast(multiset(
-    select ref(oco)
-    from xocorrencias_tab oco
-    where oco.xucs.codigo = u.codigo) as xocorrencias_tab_t);*/
-
-insert into xtiposaula_tab (id, tipo, turnos, n_aulas, horas_turno, ocorrencias)
-select id, tipo, turnos, n_aulas, horas_turno, ref(o)
+insert into xtiposaula_tab (id, tipo, codigo, ano_letivo, periodo, turnos, n_aulas, horas_turno, ocorrencias)
+select id, tipo, o.codigo, o.ano_letivo, o.periodo, turnos, n_aulas, horas_turno, ref(o)
 from GTD10.xtiposaula
 join xocorrencias_tab o
 on (o.ano_letivo = GTD10.xtiposaula.ano_letivo
@@ -36,24 +18,45 @@ on (o.ano_letivo = GTD10.xtiposaula.ano_letivo
     and o.codigo = GTD10.xtiposaula.codigo
 );
 
-insert into xdsd_tab (horas, fator, ordem, docentes, tiposaula)
-select horas, fator, ordem, tiposaula, ref(doc), cast(multiset(
-    select t.*
-    from xtiposaula_tab t
-    join xdsd d
-    where d.id = t.id) as xtiposaula_tab_t)
-from GTD10.xdsd
-join xdocentes_tab
-on xdocentes_tab.nr = GTD10.xdsd.nr;
+insert into xdsd_tab (id, nr, horas, fator, ordem, tiposaula)
+select t.id, nr, horas, fator, ordem, ref(t)
+from gtd10.xdsd
+join xtiposaula_tab t
+on gtd10.xdsd.id = t.id;
 
-/*
-insert into xdsd_tab (horas, fator, ordem, docentes, tiposaula)
-select horas, fator, ordem, tiposaula, ref(doc), cast(multiset(
+insert into xdocentes_tab (nr, nome, sigla, categoria, proprio, apelido, estado)
+select nr, nome, sigla, categoria, proprio, apelido, estado
+from gtd10.xdocentes;
+
+-- updates
+update xucs_tab u
+set u.ocorrencias = cast(multiset(
+    select ref(oco)
+    from xocorrencias_tab oco
+    where oco.codigo = u.codigo) as xocorrencias_tab_t);
+
+update xocorrencias_tab o
+set o.tiposaula = cast(multiset(
     select ref(t)
     from xtiposaula_tab t
-    join xdsd d
-    where d.id = t.id) as xtiposaula_tab_t)
-from GTD10.xdsd
-join xdocentes_tab
-on xdocentes_tab.nr = GTD10.xdsd.nr;
-*/
+    where o.ano_letivo = t.ano_letivo
+    and o.periodo = t.periodo
+    and o.codigo = t.codigo) as xtiposaula_tab_t);
+
+update xtiposaula_tab t
+set t.dsd = cast(multiset(
+    select ref(d)
+    from xdsd_tab d
+    where d.id = t.id) as xdsd_tab_t);
+
+update xdsd_tab d
+set d.docentes = (select ref(doc)
+    from xdocentes_tab doc
+    where doc.nr = d.nr); 
+
+update xdocentes_tab t
+set t.dsd = cast(multiset(
+    select ref(d)
+    from xdsd_tab d
+    where d.nr = t.nr) as xdsd_tab_t);
+
