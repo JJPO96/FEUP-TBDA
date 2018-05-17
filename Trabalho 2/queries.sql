e96295ceec76474c5fe07ce95351d2a5f6595dec
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-05-17 16:27:47 +0100

    aiii

--- Trabalho 2/queries.sql
+++ Trabalho 2/queries.sql
@@ -20,22 +20,18 @@ group by (d.tiposaula.codigo)
 having sum(d.tiposaula.horas_turno * d.tiposaula.turnos) <> sum(d.horas);
 
 -- c)
-create or replace view sum_teacher_hours as
-select distinct d.docentes.nr as nr , d.docentes.nome as nome, d.tiposaula.tipo as tipoaula, sum(d.horas * d.fator) as total_hours
-from xdsd_tab d 
-where d.tiposaula.ano_letivo = '2003/2004'
+create or replace view hours as
+select distinct d.docentes.nr as nr , d.docentes.nome as nome, d.tiposaula.tipo as tipoaula, sum(d.horas * d.fator) as total_hours,
+rank() over
+(partition by d.tiposaula.tipo order by sum(d.horas * d.fator) desc) as rank
+from xdsd_tab d
+where d.tiposaula.ano_letivo = '2003/2004' 
 group by (d.docentes.nr, d.docentes.nome,d.tiposaula.tipo);
 
-create or replace view max_hours as
-select tipoaula, max(total_hours)as max_teacher
-from sum_teacher_hours
-group by tipoaula;
+select nr,nome, tipoaula, total_hours
+from hours
+where rank=1 and total_hours is not null;
 
-select nr, nome, tipoaula, max_teacher
-from sum_teacher_hours
-join max_hours
-on sum_teacher_hours.tipoaula=max_hours.tipoaula
-and sum_teacher_hours.total_hours=max_hours.max_teacher;
 
 -- d)
 
@@ -46,12 +42,6 @@ and sum_teacher_hours.total_hours=max_hours.max_teacher;
 -- group by d.docentes.nome, d.docentes.categoria, d.tiposaula.ano_letivo;
 
 -- e)
-/*smurf*/
-select o.ucs.curso, o.periodo, o.ano_letivo, coalesce(sum(o.tiposaula.n_aulas * o.tiposaula.horas_turno),0)
-from xocorrencias_tab o
-where o.periodo like '%S' and (o.tiposaula.n_aulas is not null or o.tiposaula.horas_turno is not null)
-group by o.ucs.curso, o.periodo, o.ano_letivo;
-
 select d.tiposaula.periodo, d.tiposaula.ano_letivo, sum(d.horas)
 from xdsd_tab d
 where d.tiposaula.periodo like '%S'
