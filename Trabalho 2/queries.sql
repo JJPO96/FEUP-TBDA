b379482d0f921aba007b380c947fe1271d6bc05c
Ana Rita Torres <anaritatorres96@outlook.pt>
2018-05-19 23:24:37 +0100

    queries changed

--- Trabalho 2/queries.sql
+++ Trabalho 2/queries.sql
@@ -18,58 +18,56 @@ where ta.ocorrencias.ucs.curso = 233
 group by ta.tipo;
 
 -- b)
--- antigo
-select d.tiposaula.codigo, sum(d.tiposaula.horas_turno * d.tiposaula.turnos) as total_required_class_hours, sum(d.horas) as total_assigned_hours
+create or replace view required_hours as
+select t.codigo as code, sum(t.class_hours()) as total_class_hours_required
+from xtiposaula_tab t
+where t.ano_letivo = '2003/2004'
+group by t.codigo;
+
+create or replace view assign_hours as
+select d.tiposaula.codigo as code, sum(d.horas) as total_assigned_hours
 from xdsd_tab d
 where d.tiposaula.ano_letivo = '2003/2004'
-group by (d.tiposaula.codigo)
-having sum(d.tiposaula.horas_turno * d.tiposaula.turnos) <> sum(d.horas);
--- com metodo
-select d.tiposaula.codigo, sum(d.tiposaula.class_hours()) as total_required_class_hours, sum(d.horas) as total_assigned_hours
-from xdsd_tab d
-where d.tiposaula.ano_letivo = '2003/2004'
-group by (d.tiposaula.codigo)
-having sum(d.tiposaula.class_hours()) <> sum(d.horas);
+group by d.tiposaula.codigo;
+
+select required_hours.code, total_class_hours_required, total_assigned_hours
+from required_hours, assign_hours
+where required_hours.code = assign_hours.code and total_class_hours_required <> total_assigned_hours;
+
 
 -- c)
---antigo
 create or replace view hours as
-select distinct d.docentes.nr as nr , d.docentes.nome as nome, d.tiposaula.tipo as tipoaula, sum(d.horas * d.fator) as total_hours,
+select distinct d.docentes.nr as nr , d.docentes.nome as nome, d.tiposaula.tipo as tipoaula, sum(d.factor_hours()) as total_class_hours_times_factor,
 rank() over
-(partition by d.tiposaula.tipo order by sum(d.horas * d.fator) desc) as rank
+(partition by d.tiposaula.tipo order by sum(d.horas) desc) as rank
 from xdsd_tab d
-where d.tiposaula.ano_letivo = '2003/2004' 
-group by (d.docentes.nr, d.docentes.nome,d.tiposaula.tipo);
---com metodo
-create or replace view hours as
-select distinct d.docentes.nr as nr , d.docentes.nome as nome, d.tiposaula.tipo as tipoaula, sum(d.factor_hours()) as total_hours,
-rank() over
-(partition by d.tiposaula.tipo order by sum(d.factor_hours()) desc) as rank
-from xdsd_tab d
-where d.tiposaula.ano_letivo = '2003/2004' 
+where d.tiposaula.ano_letivo = '2003/2004'
 group by (d.docentes.nr, d.docentes.nome,d.tiposaula.tipo);
 
-select nr,nome, tipoaula, total_hours
+select nr,nome, tipoaula, total_class_hours_times_factor
 from hours
-where rank=1 and total_hours is not null;
-
+where rank=1;
 
 -- d)
 
--- Para confirmar
--- select d.docentes.nome as professor, d.tiposaula.ano_letivo as ano_letivo, d.docentes.categoria as categoria, round(avg(d.horas), 2) as average_number_hours
--- from xdsd_tab d
--- where regexp_like (d.tiposaula.ano_letivo, '^200[1-4]')
--- group by d.docentes.nome, d.docentes.categoria, d.tiposaula.ano_letivo;
+select d.tiposaula.ano_letivo as ano_letivo, d.docentes.categoria as categoria, round(avg(d.horas), 2) as average_number_hours
+from xdsd_tab d
+where regexp_like (d.tiposaula.ano_letivo, '^200[1-4]')
+group by d.docentes.categoria, d.tiposaula.ano_letivo;
 
 -- e)
-select d.tiposaula.periodo, d.tiposaula.ano_letivo, sum(d.horas)
-from xdsd_tab d
-where d.tiposaula.periodo like '%S'
-group by (d.tiposaula.periodo, d.tiposaula.ano_letivo);
+select value(t).ano_letivo as ano_letivo, value(t).periodo as semester, sum(value(t).horas_turno) as total_hours_week
+from xucs_tab u, table(u.ocorrencias) o, table(value(o).tiposaula) t
+where value(t).periodo like '%S' and value(t).class_hours() is not null
+group by value(t).ano_letivo, value(t).periodo;
+
 
 -- f)
 select o.codigo, o.aprovados, o.inscritos
 from xocorrencias_tab o
 where o.ano_letivo = '1995/1996' 
 and o.codigo = 'EM533';
+
+select doc.nome, value(d).tiposaula.ocorrencias.ucs.designacao
+from xdocentes_tab doc, table(doc.dsd) d
+where doc.nome = '%Abel' /*and value(d).tiposaula.ano_letivo = '2003/2004'*/;
