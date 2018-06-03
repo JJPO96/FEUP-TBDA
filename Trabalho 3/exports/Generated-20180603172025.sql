0680858778abf5cb2eed142a80c7e53555039135
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2018-06-03 17:22:22 +0100

    fixed encoding of exports

---
+++ Trabalho 3/exports/Generated-20180603172025.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - Domingo-Junho-03-2018   
+--------------------------------------------------------
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\ATIVIDADES.sql
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\CONCELHOS.sql
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\DISTRITOS.sql
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\RECINTOS.sql
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\REGIOES.sql
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\TIPOS.sql
+@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\USOS.sql
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\ATIVIDADES_DATA_TABLE.json
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\CONCELHOS_DATA_TABLE.json
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\DISTRITOS_DATA_TABLE.json
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\RECINTOS_DATA_TABLE.json
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\REGIOES_DATA_TABLE.json
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\TIPOS_DATA_TABLE.json
+--@C:\Users\joliveira\Documents\Universidade\4A\2S\TBDA\Trabalho 3\exports\USOS_DATA_TABLE.json
